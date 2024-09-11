414e8f8dcf6156a501e193589721339cfbb1a63e
Suyog-B20-Git <suyog7375@gmail.com>
2024-09-11 12:30:18 +0530

    Add files via upload

---
+++ JS/10_sep/script.js
@@ -0,0 +1,25 @@
+// Arithmaatic Operator
+// console.log("Add:", 10 + 5);
+// console.log("sub:", 10 - 5);
+// console.log("mul:", 10 * 5);
+// console.log("div:", 10 / 5);
+
+// Comparison Operator
+// console.log(3==5);
+// console.log(3!=5);
+// console.log(3>5);
+// console.log(3<5);
+// console.log(3>=5);
+// console.log(3<=5);
+// console.log(3===3);
+// console.log(3==="3");
+
+// Logical operator
+// console.log()
+
+// console.log(!true);
+// console.log(!true);
+
+// Assignment operator4
+
+// console.log(~6);
---
+++ JS/11_sep/script.js
@@ -0,0 +1,58 @@
+// Even Odd
+// var num = 8;
+
+// if (num % 2 == 0) {
+//     console.log("Even");
+// }
+// else {
+//     console.log("Odd");
+// }
+
+// var num = 15;
+// if (num > 10 && num < 20) {
+//     console.log("Accepted")
+// }
+// else {
+//     console.log("Rejected")
+// }
+
+// var year = 2024
+
+// if (year >= 1500) {
+//     if (year % 4 == 0) {
+//         console.log("Leap Year");
+//     }
+//     else {
+//         console.log("Its year not Leap year");
+//     }
+// }
+// else {
+//     console.log("Invalid Entry");
+// }
+// console.log("🐥"<"🥚");
+
+// var marks=8;
+// if(marks>=80){
+//     console.log("grade A");
+// }
+// else if(marks>=60 && marks<=79){
+//     console.log("grade B");
+// }
+// else if(marks>=35 && marks<=59){
+//     console.log("grade C");
+// }
+// else{
+//     console.log("FAil🤒");
+// }
+
+// var char = "A";
+// char = char.toLowerCase();
+// if (char == 'a' || char == 'e' || char == 'o' || char == 'i' || char == 'u') {
+//     console.log("Its Vowel");
+// }
+// else {
+//     console.log("Its consonant.");
+// }
+
+var num = 0;
+num == 0 ? console.log("zero") : num > 0 ? console.log("Positive") : console.log("Negative");
